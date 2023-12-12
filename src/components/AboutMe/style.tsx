7d927c82253920b628155057843da68b46638453
Carla Fernandes Ferreira Pinto <carlaf2p@gmail.com>
2023-12-12 13:59:50 -0300

    feat:acrescentando imagem fundo

--- src/components/AboutMe/style.tsx
+++ src/components/AboutMe/style.tsx
@@ -1,12 +1,16 @@
 import { Button } from "../../styles/Buttons";
 import { Div } from "../../styles/Global";
 import { styled } from "../../styles/stitches.config";
+import fundo3 from "../../utils/assets/fundo3.jpg"
+
 
 export const AboutMeStyle = styled("section", {
   backgroundColor: "$brand1",
+  backgroundImage: `url(${fundo3})`,
+  backgroundSize: "cover",
   padding: "12rem 0 8rem 0",
   backgroundRepeat: "no-repeat",
-  backgroundPosition: "bottom 1rem right 1rem",
+  backgroundPosition: "center",
   "@mobile": {
     padding: "9rem 0 6rem 0",
   },
--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,9 +1,12 @@
 import { Button } from "../../styles/Buttons";
 import { Container } from "../../styles/Global";
 import { styled } from "../../styles/stitches.config";
+import fundo3 from "../../utils/assets/fundo3.jpg"
 
 export const FooterStyle = styled("footer", {
   backgroundColor: "$brand1",
+  backgroundImage: `url(${fundo3})`,
+  backgroundSize: "cover",
   padding: "2rem 0 3rem 0",
   [`& ${Container}`]:{
       display: "flex",
